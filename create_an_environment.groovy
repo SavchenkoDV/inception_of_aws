801f76df3e84d0d75234bc71724cb813e77c0863
DESKTOP-SJPL9IE\DVSavchenko <dvs.rocket@gmail.com>
2023-01-31 13:04:33 +0300

    Update create_an_environment.groovy

--- create_an_environment.groovy
+++ create_an_environment.groovy
@@ -15,6 +15,13 @@ pipeline {
                     def ipWS = WebSite.split(';');
                     println ipWS[0]
                     println ipWS[1]
+
+                    def TEST = ipDB + ipDB
+                    int i = 0;
+                    while (i < TEST.size()) {
+                        println TEST[i];
+                        i++;
+                    }
                 }
             }
         }
