eb8de43df2061ed2239c439cf630ad23d3decbed
DESKTOP-SJPL9IE\DVSavchenko <dvs.rocket@gmail.com>
2023-01-31 20:50:30 +0300

    Update build_and_deploy.groovy

--- build_and_deploy.groovy
+++ build_and_deploy.groovy
@@ -5,10 +5,8 @@ pipeline {
     }
     environment {
         MDB_ROOT_PASSWORD = credentials('mdb_root_pass')
+        MDB_USER_PASSWORD = credentials('mdb_user_pass')
     }
-//    withCredentials([usernameColonPassword(credentialsId: 'mdb_root_pass', variable: 'MDB_ROOT_PASSWORD')]) {
-//        // some block
-//    }
     parameters {
         string( name: 'WebSite', defaultValue: 'None', description: "Enter, separated by commas, the IP address(es) of the host(s) where you want to build and deploy the WEBSITE(S). Example: 13.39.107.210;13.38.121.165")
         string( name: 'DataBase', defaultValue: 'None', description: "Enter, separated by commas, the IP address(es) of the host(s) where you want to build and deploy the DATA BASE(s). Example: 13.38.250.255")
@@ -24,7 +22,9 @@ pipeline {
                         sshagent(credentials: ['websites']) {
                             sh """
                                 ssh ubuntu@${ip} '
-                                    mysql -u root -h 172.31.34.176 --password=${MDB_ROOT_PASSWORD} -Bse "SHOW DATABASES"
+
+                                    export HN=\$(hostname -i)
+                                    mysql -u root -h \$HN --password=${MDB_ROOT_PASSWORD} -Bse "SHOW DATABASES"
                                 '
                             """
                         }
