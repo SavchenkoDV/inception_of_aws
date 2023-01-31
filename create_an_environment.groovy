f56ceca66cec5898c6f2452a36351a25986eff87
DESKTOP-SJPL9IE\DVSavchenko <dvs.rocket@gmail.com>
2023-01-31 15:29:41 +0300

    Update create_an_environment.groovy

--- create_an_environment.groovy
+++ create_an_environment.groovy
@@ -13,15 +13,33 @@ pipeline {
                     def ipDB = DataBase.split(';') as Set
                     def ipWS = WebSite.split(';') as Set
                     def ipDocker = ipDB + ipWS
-                    if (ipDocker.size() < 0)
 
-                    println ipDocker.size()
-
-                    int i = 0;
-                    while (i < ipDocker.size()) {
-                        println ipDocker[i];
-                        i++;
-                    }
+                    for (ip in ipDocker) {
+                        println ip
+                        println "DOCKER"
+//                        sshagent(credentials: ['websites']) {
+//                            sh '''
+//                                ssh-keyscan -H ${Server} >> ~/.ssh/known_hosts
+//                                ssh ubuntu@${Server} '
+//                                    sudo apt update;
+//                                    sudo apt install apt-transport-https ca-certificates curl software-properties-common -y;
+//                                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+//                                    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable";
+//                                    sudo apt update;
+//                                    apt-cache policy docker-ce;
+//                                    sudo apt install docker-ce -y;
+//                                    sudo systemctl status docker;
+//
+//                                    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
+//                                    sudo chmod +x /usr/local/bin/docker-compose;
+//                                    docker-compose --version;
+//                                    sudo apt install mysql-client-core-8.0
+//                                '
+//                            '''
+                        if (ipWS.findAll(ip))
+                            println ip
+                            println "DOCKER COMPOSE"
+                        }
 
 
                 }
